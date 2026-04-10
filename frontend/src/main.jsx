83dd62a9657077e4e96d54a2f127bf13ed8346f5
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-04-10 19:02:34 +0800

    style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './pages/LandingPage.jsx';
 
