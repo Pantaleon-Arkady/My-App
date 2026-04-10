70e9a89ecf7c56a9ab7d80d5d9b151481cf1e56c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-04-10 12:18:16 +0800

    React:   - used main.jsx as main file instead of app.jsx   - confirmed bootstrap and react bootstrap working along with react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,11 @@
-import axios from "../src/api/axios"
-import { useState, useEffect } from "react"
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-    const [message, setMessage] = useState("");
-    const [authReq, setAuthReq] = useState("");
-
-    async function AuthRequest() {
-        try {
-            const res = await axios.get("/auth-request");
-            setAuthReq(res.data.response);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    useEffect(() => {
-        axios.get("/api/test")
-            .then(res => {
-                setMessage(res.data.message);
-            })
-            .catch(err => console.error(err));
-
-        AuthRequest();
-    }, [])
 
     return (
-        <>
-            {message}
-            {authReq}
-        </>
+        <></>
     )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './pages/LandingPage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const el = document.getElementById("root");
+
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+        </Routes>
+    </BrowserRouter>
+);
