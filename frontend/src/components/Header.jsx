09077fbe4e08d457fdaf5a4682f4bc943f1c19a0
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-04-13 17:34:55 +0800

    mobile nav, looks whack

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,35 @@
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router";
+import axios from "../api/axios";
+import Mobilenav from "./Mobilenav";
+
 function Headers({ rightFeature, page }) {
+    const { user, logout } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await axios.post("/logout", {}, { withCredentials: true });
+            logout();
+            navigate("/");
+        } catch (error) {
+            const data = error.response?.data;
+            console.error(data?.message || "Logout failed");
+            alert(data?.message || "Logout failed");
+        }
+    };
+
+    const handleHome = () => {
+        navigate("/home");
+    }
 
     return (
         <div className="main_header_div bg-black px-3 py-4 text-center d-flex flex-row justify-content-start justify-content-md-between text-white">
 
             <div className="mobile_header_feature d-flex d-md-none">
+                <Mobilenav
+                />
             </div>
 
             <div className="header-app-name text-center text-md-start fs-2 fw-bold d-flex flex-row justify-content-between">
@@ -17,6 +43,18 @@ function Headers({ rightFeature, page }) {
             </div>
 
             <div className="w-25 flex-row justify-content-around d-none d-md-flex">
+                <button
+                    onClick={handleHome}
+                    className="btn btn-outline-primary"
+                >
+                    Home
+                </button>
+                <button
+                    className="btn btn-outline-danger"
+                    onClick={handleLogout}
+                >
+                    Logout
+                </button>
             </div>
 
         </div>
---
+++ frontend/src/components/Mobilenav.jsx
@@ -0,0 +1,84 @@
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
+
+function Mobilenav() {
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const navTab = useRef(null);
+    const closeNav = useRef(null);
+    const [nav, setNav] = useState(false);
+
+    const handleLogout = async () => {
+        try {
+            await axios.post("/logout", {}, { withCredentials: true });
+            logout();
+            navigate("/");
+        } catch (error) {
+            const data = error.response?.data;
+            console.error(data?.message || "Logout failed");
+            alert(data?.message || "Logout failed");
+        }
+    };
+
+    const handleHome = () => {
+        navigate("/home");
+    }
+
+    const handlePosts = () => {
+        navigate("/post");
+    }
+
+    return (
+        user ?
+            <>
+                <button
+                    className="btn btn-outline-light p-2 bg-white"
+                    onClick={() => setNav(!nav)}
+                >
+                    <img
+                        src="/statics/three-v-dot.svg"
+                        alt=""
+                        className="w-7 h-7 bg-white p-1.5 rounded-md block"
+                    />
+                </button>
+                {nav && (
+                    <div
+                        ref={navTab}
+                        className="nav_tab position-fixed top-0 start-0 w-100 h-100 bg-dark bg-opacity-75"
+                    >
+                        <div
+                            className="w-75 d-flex flex-column p-2 bg-black h-100"
+                        >
+                            <div className="d-flex justify-content-end pb-3">
+                                <button
+                                    ref={closeNav}
+                                    onClick={() => setNav(false)}
+                                    className="btn btn-outline-light"
+                                >
+                                    X
+                                </button>
+                            </div>
+                            <button
+                                onClick={handleHome}
+                                className="btn btn-outline-primary mb-3"
+                            >
+                                Home
+                            </button>
+                            <button
+                                onClick={handleLogout}
+                                className="btn btn-outline-danger mb-3"
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </>
+            :
+            <></>
+    )
+}
+
+export default Mobilenav;
