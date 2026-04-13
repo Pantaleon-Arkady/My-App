0793bc7b262ca5b45eabe71c67032766ae383513
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-04-13 16:51:46 +0800

    login form added and logic to toggle between forms

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,18 @@
 import RegisterForm from "../forms/RegisterForm";
+import LoginForm from "../forms/LoginForm";
 import Headers from "../components/Header";
+import { useState } from "react";
 
 function LandingPage() {
+    const [isRegister, setIsRegister] = useState(false);
+
+    const showLogIn = () => {
+        setIsRegister(false);
+    };
+
+    const showRegister = () => {
+        setIsRegister(true);
+    };
 
     return (
         <div>
@@ -15,10 +26,24 @@ function LandingPage() {
                         <p className="text-center landing_greetings">
                             This app serves as a tracker and note for your workout progess, it helps for easier readability and visualizations of your workouts.
                         </p>
+                        <label className="d-none d-md-block">
+                            <a onClick={showLogIn}>Log in </a>
+                            or
+                            <a onClick={showRegister}> Register?</a>
+                        </label>
                     </div>
                     <div className="px-3 w-100 w-md-50">
-                        <RegisterForm />
+                        {isRegister?
+                            <RegisterForm/>
+                            :
+                            <LoginForm />
+                        }
                     </div>
+                    <label className="mt-3  d-md-none">
+                        <a onClick={showLogIn}>Log in </a>
+                        or
+                        <a onClick={showRegister}> Register?</a>
+                    </label>
                 </div>
             </div>
         </div>
