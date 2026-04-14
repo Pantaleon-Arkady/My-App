3a82f5c20edd8a52f17976fdd14f41ad7cc8509e
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-04-14 09:29:49 +0800

    Logout backend

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-
+Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
 
